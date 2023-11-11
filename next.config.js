ec06fbd1794830f7172db236ea739f0eec68af52
mydemilade <ayunoluwademilade@gmail.com>
2023-11-11 13:22:42 +0100

    car showcase

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains: [ 'cdn.imagin.studio']
-    }
-}
-
-module.exports = nextConfig
+    images: {
+      domains: ['cdn.imagin.studio'],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
